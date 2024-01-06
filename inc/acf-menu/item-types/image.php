f12253572e46731967d790934a878083ad6b9610
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-06 17:50:12 +0100

    menu items  align

--- inc/acf-menu/item-types/image.php
+++ inc/acf-menu/item-types/image.php
@@ -4,12 +4,12 @@
 		$image_size = 'full';
 		$max_width = $image['width']; 
 		
-		$align_left_class = get_sub_field('align_to_left') ? 'main-nav__align-left' : '';
+		$align_left_class = get_sub_field('align_to_left') ? 'main-nav__list-item--left' : '';
 		$custom_classes = get_sub_field('custom_classes');
         $id_attribute = ($custom_id = get_sub_field('custom_id')) ? 'id="' . $custom_id . '"' : '';
 ?>
 		
-	<li <?= $id_attribute; ?> class="main-nav__image <?= $align_left_class . ' ' . esc_html($custom_classes); ?>">
+	<li <?= $id_attribute; ?> class="main-nav__list-item main-nav__image <?= $align_left_class . ' ' . esc_html($custom_classes); ?>">
 		<img class="" <?php acf_responsive_image($image_id,$image_size,$max_width); ?> alt="" />
 	</li>
 
