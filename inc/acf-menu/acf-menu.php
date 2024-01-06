47895a9f85100a02a4629e300f7c3c9bde1d25cc
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-06 15:12:44 +0100

    menu image working

--- inc/acf-menu/acf-menu.php
+++ inc/acf-menu/acf-menu.php
@@ -1,11 +1,14 @@
 <?php
-    if( have_rows('main_menu', 'options') ):
-        while( have_rows('main_menu', 'options') ) : the_row();
-            $menu_item_type = get_sub_field('menu_item_type');
+
+require_once get_template_directory() . '/inc/acf-menu/acf-menu-helper-functions.php';  
+    
+if( have_rows('main_menu', 'options') ):
+    while( have_rows('main_menu', 'options') ) : the_row();
+        $menu_item_type = get_sub_field('menu_item_type');
             
-            $possible_file = get_template_directory() . '/inc/acf-menu/item-types/' . $menu_item_type . '.php';
-            if( file_exists( $possible_file ) ){
-                include( $possible_file );
-            }
-        endwhile;
-    endif;
+        $possible_file = get_template_directory() . '/inc/acf-menu/item-types/' . $menu_item_type . '.php';
+        if( file_exists( $possible_file ) ){
+            include( $possible_file );
+        }
+    endwhile;
+endif;
