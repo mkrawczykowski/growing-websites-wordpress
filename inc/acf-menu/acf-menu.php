69ccfb82e7e7765e0a2b080ab806430aa5c9cf51
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-07 18:02:15 +0100

    mega menu list item

--- inc/acf-menu/acf-menu.php
+++ inc/acf-menu/acf-menu.php
@@ -5,7 +5,7 @@ require_once get_template_directory() . '/inc/acf-menu/acf-menu-helper-functions
 if( have_rows('main_menu', 'options') ):
     while( have_rows('main_menu', 'options') ) : the_row();
         $menu_item_type = get_sub_field('menu_item_type');
-            
+        echo $menu_item_type;  
         $possible_file = get_template_directory() . '/inc/acf-menu/item-types/' . $menu_item_type . '.php';
         if( file_exists( $possible_file ) ){
             include( $possible_file );
---
+++ inc/acf-menu/item-types/mega_menu_panel_1.php
@@ -0,0 +1,16 @@
+<?php 
+
+    if ($mega_menu_panel_1 = get_sub_field('mega_menu_panel_1')) :
+
+        $menu_item_label = $mega_menu_panel_1['menu_item_label'];
+        $custom_classes = get_sub_field('custom_classes');
+        $id_attribute = ($custom_id = get_sub_field('custom_id')) ? 'id="' . $custom_id . '"' : ''; 
+?>
+        <li <?= $id_attribute; ?> class="main-nav__list-item main-nav__mega-menu-panel-1 <?= esc_html($custom_classes); ?>">
+            <span class="">
+                <?php echo esc_html( $menu_item_label ); ?>
+            </span>
+        </li>
+    <?php
+    endif;
+?>
