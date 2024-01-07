4ba9b061a5fd805033b524a36741ae63c2ba6b18
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-07 19:35:21 +0100

    item with children hover works

--- src/js/main-menu.js
+++ src/js/main-menu.js
@@ -4,14 +4,23 @@ document.addEventListener('DOMContentLoaded', function(){
 
     if (mainMenuItemHasChildren.length){
         mainMenuItemHasChildren.forEach(itemHasChildren=>{
-            itemHasChildren.addEventListener('mouseover', function(){
+            itemHasChildren.addEventListener('mouseover', function(event){
+               
                 if(!coveringLayer.classList.contains('active')){
                     coveringLayer.classList.add('active');
+                    console.log('test')
+                }
+                if(!itemHasChildren.classList.contains('active')){
+                    itemHasChildren.classList.add('active');
                 }
             })
-            itemHasChildren.addEventListener('mouseout', function(){
+            itemHasChildren.addEventListener('mouseout', function(event){
+                
                 if(coveringLayer.classList.contains('active')){
                     coveringLayer.classList.remove('active');
+                }
+                if(itemHasChildren.classList.contains('active')){
+                    itemHasChildren.classList.remove('active');
                 }
             })
         })
