355d9d7755b151e677d851acfea65b791ca73ac8
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-12 07:52:05 +0100

    image sizes

--- inc/registers.php
+++ inc/registers.php
@@ -1,5 +1,7 @@
 <?php
-  // add_image_size('image-fullhd', 1920, 9999, array('center', 'center'));
+  add_image_size('1920-3x', 5760, 9999, array('center', 'center'));
+  add_image_size('1920-2x', 3840, 9999, array('center', 'center'));
+  add_image_size('1920-1x', 1920, 9999, array('center', 'center'));
 
   add_theme_support( 'post-thumbnails' );
 ?>
