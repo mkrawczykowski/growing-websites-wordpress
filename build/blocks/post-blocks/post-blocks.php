dbe78d17ea446b17df99d354f93005fa662a0bbb
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-16 09:28:32 +0100

    post-blocks previous_posts

--- build/blocks/post-blocks/post-blocks.php
+++ build/blocks/post-blocks/post-blocks.php
@@ -7,9 +7,8 @@
   $posts_in_section = get_field('posts_in_section');
 
   if (!function_exists('the_featured_posts')){
-    function the_featured_posts($featured_posts){
-      foreach($featured_posts as $featured_post) : 
-        $featured_post_id = $featured_post->ID;
+    function the_featured_posts($featured_posts_ids){
+      foreach($featured_posts_ids as $featured_post_id) : 
         $featured_post_categories = get_the_category($featured_post_id);
         ?>
         <div class="post">
@@ -31,14 +30,29 @@
 
               <div class="post__data"><?= get_the_date('F d Y', $featured_post_id); ?></div>
             </div>
-            <h3 class="post__title"><?= $featured_post->post_title; ?></h3>
-            <p class="post__title"><?= $featured_post->post_excerpt; ?></p>
+            <h3 class="post__title"><?= get_the_title($featured_post_id); ?></h3>
+            <p class="post__title"><?= get_the_excerpt($featured_post_id) ?></p>
           </div>
           <div class="post_image">
             <img class="" <?php acf_responsive_image(get_post_thumbnail_id($featured_post_id),'full',1024); ?> alt="" />
           </div>
         </div>
       <?php endforeach;
+    }
+  }
+
+  if (!function_exists('the_previous_posts')){
+    function the_previous_posts(){
+
+
+
+      $latest_cpt = get_posts("post_type=project&numberposts=3");
+      // print_r($latest_cpt);
+      foreach ($latest_cpt as $post){
+        echo $post->ID;
+      }
+
+
     }
   }
 
@@ -51,18 +65,18 @@
     <?= $heading ? '<h2 class="post-blocks__heading">' . $heading . '</h2>' : ''; ?>
 
     <?php 
-    if ($posts_in_section){
       switch ($which_posts) {
         case 'previous_posts' :
-          echo 'previous_posts';
+          the_previous_posts();
         break;
 
         case 'featured_posts' :
-          the_featured_posts($posts_in_section);
+          if ($posts_in_section){
+            the_featured_posts($posts_in_section);
+          }
         break;
-      }      
-    }
-
+      }
     ?>
+
   </div>
 </section>
