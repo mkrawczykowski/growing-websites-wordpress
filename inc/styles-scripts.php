f8c4591b2bd08e5763bdade32cae5da28cb43db6
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-18 20:59:02 +0100

    RM

--- inc/styles-scripts.php
+++ inc/styles-scripts.php
@@ -11,42 +11,20 @@ function my_scripts(){
   wp_enqueue_style('main-style', get_stylesheet_directory_uri() . '/build/theme/index.css');
   wp_enqueue_script('main-scripts', get_stylesheet_directory_uri() . '/build/theme/index.js');
 
-
   $global_blocks_array = $GLOBALS['gw_gutenberg_blocks_array_with_scripts_gw'];
 
   if (is_array($global_blocks_array)){
     foreach($global_blocks_array as $block){
-        // var_dump($global_blocks_array);
-        $global_scripts_array = $block['block-scripts'];
+        
+      $global_scripts_array = $block['block-scripts'];
 
-        if (is_array($global_scripts_array)){
-          foreach($global_scripts_array as $script){
-
-            
-// $status = 'enqueued';
-// if (wp_script_is($script, $status )) {
-//     return;
-// } else {
-//   wp_enqueue_script($script, get_stylesheet_directory_uri() . '/build/scripts/' . $script . '.js');
-//     // wp_register_script( 'fluidVids.js', plugin_dir_url(__FILE__).'js/fluidvids.min.js');
-//     // wp_enqueue_script( 'fluidVids.js' );
-// }
-wp_enqueue_script($script, get_stylesheet_directory_uri() . '/build/scripts/' . $script . '.js'); 
-          }
+      if (is_array($global_scripts_array)){
+        foreach($global_scripts_array as $script){
+          wp_enqueue_script($script, get_stylesheet_directory_uri() . '/build/scripts/' . $script . '.js'); 
         }
-          
-        // var_dump($global_scripts_array);
-        // echo '<br>';
-        // echo '======================';
-        // echo '<br>';
+      }
     }
   }
-
-  // if (is_array($global_scripts_array)){
-  //   foreach($global_scripts_array as $script){
-  //     wp_enqueue_script($script, get_stylesheet_directory_uri() . '/build/scripts/' . $script . '.js');
-  //   }  
-  // }
   
 }
 add_action('wp_enqueue_scripts', 'my_scripts');
