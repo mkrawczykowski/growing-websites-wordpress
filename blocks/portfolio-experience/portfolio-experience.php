c7450003db20be1bdb6edc9786862e678ebaa5d9
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-20 19:38:36 +0100

    button and headings

--- blocks/portfolio-experience/portfolio-experience.php
+++ blocks/portfolio-experience/portfolio-experience.php
@@ -9,19 +9,22 @@
 
 <section class="portfolio-experience">
     <div class="container">
-        <ul class="years-filter">
-            <li class="years-filter__item active" data-years-filter>2022</li>
-            <li class="years-filter__item active" data-years-filter>2021</li>
-            <li class="years-filter__item active" data-years-filter>2020</li>
-            <li class="years-filter__item active" data-years-filter>2019</li>
-            <li class="years-filter__item active" data-years-filter>2018</li>
-            <li class="years-filter__item active" data-years-filter>2017</li>
-            <li class="years-filter__item active" data-years-filter>2016</li>
-            <li class="years-filter__item" data-years-filter>2015</li>
-            <li class="years-filter__item" data-years-filter>2014</li>
-            <li class="years-filter__item" data-years-filter>2013</li>
-            <li class="years-filter__item" data-years-filter>2012</li>
-        </ul>
+        <div class="years-filter">
+        <h4 class="years-filter__heading">project year</h4>
+            <ul class="years-filter__list">
+                <li class="years-filter__item active" data-years-filter>2022</li>
+                <li class="years-filter__item active" data-years-filter>2021</li>
+                <li class="years-filter__item active" data-years-filter>2020</li>
+                <li class="years-filter__item active" data-years-filter>2019</li>
+                <li class="years-filter__item active" data-years-filter>2018</li>
+                <li class="years-filter__item active" data-years-filter>2017</li>
+                <li class="years-filter__item active" data-years-filter>2016</li>
+                <li class="years-filter__item" data-years-filter>2015</li>
+                <li class="years-filter__item" data-years-filter>2014</li>
+                <li class="years-filter__item" data-years-filter>2013</li>
+                <li class="years-filter__item" data-years-filter>2012</li>
+            </ul>
+        </div>
         <div class="filters-columns">
 
 
@@ -36,20 +39,21 @@
                         <option value="2019">2019</option>
                     </select>
                     <div class="dropdown-checkboxes__active-wrapper">
+                        <h4 class="dropdown-checkboxes__heading">project type</h4>
                         <ul class="dropdown-checkboxes__active-list" data-active-list>
                             <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="blogs">blogs</li>
                             <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="online stores">online stores</li>
                             <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="user interfaces">user interfaces</li>
                         </ul>
                     </div>
-                    <!-- <ul class="dropdown-checkboxes__choices-list" data-choices-list>
+                    <ul class="dropdown-checkboxes__choices-list" data-choices-list>
                         <li class="dropdown-checkboxes__choices-list-item" data-item-type="choice" data-value="websites">websites</li>
-                    </ul> -->
+                    </ul>
                 </div>
             </div>
 
 
-            <div class="filters__filter">
+            <div class="filters-columns__filter">
                 <div class="dropdown-checkboxes">
                     <select class="dropdown-checkboxes__select">
                         <option value="2022">2022</option>
@@ -60,6 +64,7 @@
                         <option value="2019">2019</option>
                     </select>
                     <div class="dropdown-checkboxes__active-wrapper">
+                    <h4 class="dropdown-checkboxes__heading">technologies/solutions used</h4>
                         <ul class="dropdown-checkboxes__active-list" data-active-list>
                             <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="WooCommerce">WooCommerce</li>
                             <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="WordPress">WordPress</li>
@@ -70,41 +75,13 @@
                             <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="ACF Pro">ACF Pro</li>
                         </ul>
                     </div>
-                    <!-- <ul class="dropdown-checkboxes__choices-list" data-choices-list>
+                    <ul class="dropdown-checkboxes__choices-list" data-choices-list>
                         <li class="dropdown-checkboxes__choices-list-item" data-item-type="choice" data-value="websites">websites</li>
-                    </ul> -->
+                    </ul>
                 </div>
             </div>
-
-
-            <!-- <div class="filters__filter">
-                <div class="dropdown-checkboxes">
-                    <select class="dropdown-checkboxes__select">
-                        <option value="2022">2022</option>
-                        <option value="2021">2021</option>
-                        <option value="2020">2020</option>
-                        <option value="2019">2019</option>
-                        <option value="2019">2019</option>
-                        <option value="2019">2019</option>
-                    </select>
-                    <div class="dropdown-checkboxes__active-wrapper">
-                        <ul class="dropdown-checkboxes__active-list" data-active-list>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2022">2022</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2021">2021</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2020">2020</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2019">2019</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2018">2018</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2017">2017</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2016">2016</li>
-                        </ul>
-                    </div>
-                    <!-- <ul class="dropdown-checkboxes__choices-list" data-choices-list>
-                        <li class="dropdown-checkboxes__choices-list-item" data-item-type="choice" data-value="websites">websites</li>
-                    </ul> -->
-                </div>
-            </div> -->
-
-            
+                
         </div>
+        <a class="button" href="#">Apply filters</a>
     </div>
 </section>
