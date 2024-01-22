a188faf32c36fac7368eda1e98c035d2e2836d78
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-01-22 21:57:38 +0100

    testing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
 const glob = require('glob');
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
+// const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
 
-module.exports = {
-    ...defaultConfig,
-    ...{
-        entry: () => {
-        const basePath = './src/scripts';
-        const entryFiles = glob.sync(path.join(basePath, '*.js'));
-        console.log(entryFiles);
+ const test = glob.sync(path.resolve(__dirname, './src/scripts/*.js')).reduce(function(obj, el){
+    obj[path.parse(el).name] = el;
+    return obj
+ },{})
+ console.log(test);
+
+// const basePath = './src/scripts';
+// const entryFiles = glob.sync(path.resolve(__dirname, './src/scripts/*.js'));
+//         console.log(entryFiles);
+
+// module.exports = {
+//     ...defaultConfig,
+//     ...{
+//         entry: () => {
+//         const basePath = './src/scripts';
+//         const entryFiles = glob.sync(path.join(basePath, '*.js'));
+//         console.log(entryFiles);
         // const entry = {};
 
         // entryFiles.forEach((file) => {
@@ -18,10 +28,10 @@ module.exports = {
         // });
 
         // return entry;
-    },
+    // },
     // output: {
     //     filename: 'build/scripts/[name].js',
     //     path: path.resolve(__dirname),
     // },    
-    }
-};
+    // }
+// };
