56870c74d980886c3946549023bc46a1816d2fde
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-02-11 16:51:56 +0100

    menu image link + alt

--- inc/acf-menu/item-types/image.php
+++ inc/acf-menu/item-types/image.php
@@ -1,8 +1,11 @@
 <?php 
+	$image_link = get_sub_field('link');
 	if ($image = get_sub_field($menu_item_type)) :
+		var_dump($image) ;
+		$image_alt = $image['alt'];
 		$image_id = $image['id'];
 		$image_size = 'full';
-		$max_width = $image['width']; 
+		$max_width = $image['width'];
 		
 		$align_left_class = get_sub_field('align_to_left') ? 'main-nav__list-item--left' : '';
 		$custom_classes = get_sub_field('custom_classes');
@@ -10,7 +13,9 @@
 ?>
 		
 	<li <?= $id_attribute; ?> class="main-nav__list-item logo main-nav__image <?= $align_left_class . ' ' . esc_html($custom_classes); ?>">
-		<img class="" <?php acf_responsive_image($image_id,$image_size,$max_width); ?> alt="" />
+		<?php echo $image_link ? '<a href="' . $image_link . '">' : ''; ?>
+			<img class="" <?php acf_responsive_image($image_id,$image_size,$max_width); ?> alt="<?= $image_alt; ?>" />
+		<?php echo $image_link ? '</a>' : ''; ?>
 	</li>
 
 <?php
