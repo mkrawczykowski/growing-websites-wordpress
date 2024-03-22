8965d7b6ad7dfced4d979afa3d063a05707b1630
Michal Krawczykowski <mkrawczykowski@gmail.com>
2024-03-22 12:57:09 +0100

    portfolio-experience.php

--- build/blocks/portfolio-experience/portfolio-experience.php
+++ build/blocks/portfolio-experience/portfolio-experience.php
@@ -1,110 +1,144 @@
 <?php defined('ABSPATH') or die; ?>
 
 <?php
-//   $heading = get_field('heading');
-//   $which_posts = get_field('which_posts');
-//   $post_type = get_field('post_type');
-//   $posts_in_section = get_field('posts_in_section');
+  $posts_per_page = get_field('posts_per_page', false, true, true);
+  $portfolio_filters = get_field('portfolio_filters', false, true, true);
+  $GLOBALS['global_active_portfolio_filters'] = [];
+
+  //margins for the block
+  $margin_top_small = get_field('margin_top_small', false, true, true);
+  $margin_bottom_small = get_field('margin_bottom_small', false, true, true);
+  $breakpoints = get_field('breakpoints', false, true, true);
+  $class_name = 'portfolio-experience';
+    
+  $random_class_name = random_class_name($class_name);
+  generate_margins_styles_for_section($random_class_name, $margin_top_small, $margin_bottom_small, $breakpoints);
+  $all_class_names = $class_name . ' ' . $random_class_name;
+
+  $default_posts_per_page = get_option( 'posts_per_page' );
 ?>
 
-<section class="portfolio-experience">
+<section class="<?= $all_class_names; ?> js-portfolio-experience" data-posts-per-page="<?= $default_posts_per_page; ?>" data-rest-url="<?= site_url() . '/wp-json/wp/v2/project/?' ?>">
     <div class="container">
-        <ul class="years-filter">
-            <li class="years-filter__item active" data-years-filter>2022</li>
-            <li class="years-filter__item active" data-years-filter>2021</li>
-            <li class="years-filter__item active" data-years-filter>2020</li>
-            <li class="years-filter__item active" data-years-filter>2019</li>
-            <li class="years-filter__item active" data-years-filter>2018</li>
-            <li class="years-filter__item active" data-years-filter>2017</li>
-            <li class="years-filter__item active" data-years-filter>2016</li>
-            <li class="years-filter__item" data-years-filter>2015</li>
-            <li class="years-filter__item" data-years-filter>2014</li>
-            <li class="years-filter__item" data-years-filter>2013</li>
-            <li class="years-filter__item" data-years-filter>2012</li>
-        </ul>
-        <div class="filters-columns">
+        <div class="portfolio-experience__filters">
+            <?php
+                if( have_rows('portfolio_filters') ):
+                    while( have_rows('portfolio_filters') ) : the_row();
+                        $filter_name = get_sub_field('filter_name', false, true, true);
+                        $taxonomy_slug = get_sub_field('taxonomy_slug', false, true, true); //works fine
+                        $this_taxonomy_exists = taxonomy_exists($taxonomy_slug);
+                        $filter_type = get_sub_field('filter_type', false, true, true);
+                        $template_path =  'template-parts/filters/filter-'. $filter_type. '.php';
+                        $template_exists = locate_template($template_path, false, false);
+                        $items_in_reverse_order = get_sub_field('items_in_reverse_order', false, true, true);
+                        $enable_change_filtering_type = get_sub_field('enable_change_filtering_type', false, true, true);
+                        $default_filtering_type = get_sub_field('default_filtering_type', false, true, true);
+                        $default_filtering_type_label = get_sub_field('default_filtering_type_label', false, true, true);
 
+                        if ($this_taxonomy_exists && $template_exists) : ?>
 
-            <div class="filters-columns__filter">
-                <div class="dropdown-checkboxes">
-                    <select class="dropdown-checkboxes__select">
-                        <option value="2022">2022</option>
-                        <option value="2021">2021</option>
-                        <option value="2020">2020</option>
-                        <option value="2019">2019</option>
-                        <option value="2019">2019</option>
-                        <option value="2019">2019</option>
-                    </select>
-                    <div class="dropdown-checkboxes__active-wrapper">
-                        <ul class="dropdown-checkboxes__active-list" data-active-list>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="blogs">blogs</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="online stores">online stores</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="user interfaces">user interfaces</li>
-                        </ul>
-                    </div>
-                    <!-- <ul class="dropdown-checkboxes__choices-list" data-choices-list>
-                        <li class="dropdown-checkboxes__choices-list-item" data-item-type="choice" data-value="websites">websites</li>
-                    </ul> -->
-                </div>
-            </div>
+                            <?php
+                                $active_terms_in_this_filter = get_sub_field('active_terms_in_this_filter', false, true, true);
+                                $active_terms_in_this_filter_array = get_existing_terms_list(explode(",", $active_terms_in_this_filter), $taxonomy_slug);
+                                $container_width = get_sub_field('full_width') ? 'full-width' : 'half-width';
+                            ?>
 
+                            <div class="portfolio-experience__filters-container <?= $container_width ?>">
+                                <?php
+                                    $args = [
+                                        'filter_name' => $filter_name,
+                                        'active_terms_in_this_filter_array' => $active_terms_in_this_filter_array,
+                                        'taxonomy_slug' => $taxonomy_slug,
+                                        'items_in_reverse_order' => $items_in_reverse_order,
+                                        'enable_change_filtering_type' => $enable_change_filtering_type,
+                                        'default_filtering_type' => $default_filtering_type,
+                                        'default_filtering_type_label' => $default_filtering_type_label,
+                                    ];
 
-            <div class="filters__filter">
-                <div class="dropdown-checkboxes">
-                    <select class="dropdown-checkboxes__select">
-                        <option value="2022">2022</option>
-                        <option value="2021">2021</option>
-                        <option value="2020">2020</option>
-                        <option value="2019">2019</option>
-                        <option value="2019">2019</option>
-                        <option value="2019">2019</option>
-                    </select>
-                    <div class="dropdown-checkboxes__active-wrapper">
-                        <ul class="dropdown-checkboxes__active-list" data-active-list>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="WooCommerce">WooCommerce</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="WordPress">WordPress</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="SCSS">SCSS</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="Quick.Cart">Quick.Cart</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="GeneratePress">GeneratePress</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="custom design">custom design</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="ACF Pro">ACF Pro</li>
-                        </ul>
-                    </div>
-                    <!-- <ul class="dropdown-checkboxes__choices-list" data-choices-list>
-                        <li class="dropdown-checkboxes__choices-list-item" data-item-type="choice" data-value="websites">websites</li>
-                    </ul> -->
-                </div>
-            </div>
+                                    echo get_template_part('template-parts/filters/filter', $filter_type, $args);
+                                ?>
+                            </div>
+                        <?php endif;
 
-
-            <!-- <div class="filters__filter">
-                <div class="dropdown-checkboxes">
-                    <select class="dropdown-checkboxes__select">
-                        <option value="2022">2022</option>
-                        <option value="2021">2021</option>
-                        <option value="2020">2020</option>
-                        <option value="2019">2019</option>
-                        <option value="2019">2019</option>
-                        <option value="2019">2019</option>
-                    </select>
-                    <div class="dropdown-checkboxes__active-wrapper">
-                        <ul class="dropdown-checkboxes__active-list" data-active-list>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2022">2022</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2021">2021</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2020">2020</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2019">2019</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2018">2018</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2017">2017</li>
-                            <li class="dropdown-checkboxes__active-list-item" data-item-type="active" data-value="2016">2016</li>
-                        </ul>
-                    </div>
-                    <!-- <ul class="dropdown-checkboxes__choices-list" data-choices-list>
-                        <li class="dropdown-checkboxes__choices-list-item" data-item-type="choice" data-value="websites">websites</li>
-                    </ul> -->
-                </div>
-            </div> -->
-
-            
+                        if (!$this_taxonomy_exists || !$template_exists)  : ?>
+                        <?php endif;
+                    endwhile;
+                endif;
+            ?>            
         </div>
+
+        <button class="button" href="#" id="js-portfolio-experience-apply-filters">Apply filters</button> 
     </div>
+
+    <?php
+        $posts = array();
+        $global_active_portfolio_filters = $GLOBALS['global_active_portfolio_filters'];
+        $active_filters_taxonomies = array_keys($global_active_portfolio_filters);
+
+        if ($active_filters_taxonomies) :
+            foreach($active_filters_taxonomies as $index=>$active_filters_taxonomy) :
+                $default_filtering_type = $global_active_portfolio_filters[$active_filters_taxonomy]['default_filtering_type'];
+                $args = array(
+                    'post_type'   => 'project',
+                    'fields'      => 'ids',
+                    'numberposts' => -1,
+                    'tax_query'   => array(
+                    'relation'    => $default_filtering_type,
+                    )
+                );
+                
+                $active_terms_ids = $global_active_portfolio_filters[$active_filters_taxonomy]['active_terms_ids'];
+
+                foreach ($active_terms_ids as $active_term_id){
+                    $args['tax_query'][] = array(
+                        'taxonomy' => $active_filters_taxonomy,
+                        'field'    => 'term_id',
+                        'terms'    => $active_term_id,
+                    );
+                }
+                
+                $array_of_taxonomies_posts[$index] = get_posts($args);
+
+            endforeach;
+            $filtered_posts = find_common_elements($array_of_taxonomies_posts);
+        endif;
+
+        $filtered_posts_length = count($filtered_posts);
+        $projects_string = '';
+        if ($filtered_posts_length === 1){
+            $projects_string = 'project';
+        }
+        if ($filtered_posts_length > 1){
+            $projects_string = 'projects';
+        }
+        $number_of_posts = $filtered_posts_length ? $filtered_posts_length : '0';
+        $links_in_pagination = $number_of_posts / $default_posts_per_page;
+    ?>
+
+    <div class="posts-list">
+        <?php
+        
+        if ($filtered_posts){
+            for ($i = 1; $i < $default_posts_per_page-1; $i++){
+                get_template_part('template-parts/post','box',
+                    array(
+                        'post_id'           => $filtered_posts[$i],
+                        'date'              => 'year',
+                        'category_taxonomy' => 'project-category',
+                        'tag_taxonomy'      => 'project-tag',
+                    )
+                );
+            }
+        }
+        ?>
+    </div>
+    <div class="pagination">
+            <?php
+                for ($i = 0; $i < ceil($links_in_pagination); $i++){
+                    $button_class = 'pagination__button';
+                    $i == 0 ? $button_class = 'pagination__button pagination__button--active' : NULL;
+                    echo '<button data-pagination-page-number="' . $i+1 . '" class="' . $button_class . '">' . $i+1 . '</button>';
+                }
+            ?>   
+        </div>
 </section>
